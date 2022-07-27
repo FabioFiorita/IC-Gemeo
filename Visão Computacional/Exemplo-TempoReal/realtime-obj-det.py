a1544966c101077082b2590180d135235a276050
Fabio Fiorita <fabiolfp@gmail.com>
2022-07-27 09:35:34 -0300

    Combinando os codigos em um unico programa para utilizar no robodk

--- Visão Computacional/Exemplo-TempoReal/realtime-obj-det.py
+++ Visão Computacional/Exemplo-TempoReal/realtime-obj-det.py
@@ -1,7 +1,6 @@
-from robodk.robolink import *      # RoboDK's API
-from robodk.robomath import *      # Math toolbox for robots
 import cv2
 import imutils
+from shapedetector import ShapeDetector
 
 class ShapeDetector:
 	def __init__(self):
@@ -81,32 +80,7 @@ def detectShape():
         cv2.waitKey(0)
     return shape
 
-RL = Robolink()
-
-robot = RL.Item('KUKA KR 6 R900 sixx')
-
-app_InspectA = RL.Item('App_InspectA')
-retract_InspectA = RL.Item('Retract_InspectA')
-
-def moveRobot(shape):
-    robot.MoveJ(app_InspectA)
-    if shape == "triangle":
-        moveTriangle()
-    else:
-        moveSquare()
-    robot.MoveJ(retract_InspectA)
-    
-def moveTriangle():
-    for i in range(1, 4):
-        targetName = f"Triangle {i}"
-        robot.MoveJ(RL.Item(targetName))
-    robot.MoveJ(RL.Item("Triangle 1"))
-
-def moveSquare():
-    for i in range(1, 5):
-        targetName = f"Square {i}"
-        robot.MoveJ(RL.Item(targetName))
-    robot.MoveJ(RL.Item("Square 1"))
-
 shape = detectShape()
-moveRobot(shape)
+
+
+
--- Visão Computacional/Exemplo-TempoReal/shapedetector.py
+++ Visão Computacional/Exemplo-TempoReal/shapedetector.py
@@ -11,10 +11,10 @@ class ShapeDetector:
 		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
 		# if the shape is a triangle, it will have 3 vertices else it's a rectangle
-        if len(approx) == 3:
+		if len(approx) == 3:
 			shape = "triangle"
-        else:
-			shape = "rectangle"
+		else:
+			shape = "square"
 
 		# return the name of the shape
-        return shape
+		return shape
