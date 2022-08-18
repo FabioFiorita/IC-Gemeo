1fdee57913899485dc4c5caf04cdea9abec22257
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-18 14:33:45 -0300

    wip: Localizacao de objetos

--- Localização de Objetos/image_position.py
+++ Localização de Objetos/image_position.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-path = 'Localização de Objetos/Exemplo/bancada1.png'
-
 def getElements(image):
     imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower = np.array([32, 0, 0])
@@ -17,9 +15,14 @@ def showImages(images):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-def getPosition(image):
-    img = image.copy()
-    im_bw = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+def getPosition():
+    path = 'Localização de Objetos/Exemplo/bancada1.png'
+    image = cv2.imread(path)
+    #image = cv2.resize(image, (1200, 2000))
+    maskedImage = getElements(image)
+    
+    img = maskedImage.copy()
+    im_bw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     #blur = cv2.GaussianBlur(im_bw, (5,5), 0)
     #im_bw = cv2.Canny(blur, 10, 90)
     contours, hierarchy = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -29,17 +32,9 @@ def getPosition(image):
         if M['m00'] != 0:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            #if cx < 200 and cy < 200:
             cv2.drawContours(img, [c], -1, (0,255,0), 3)
             cv2.circle(img, (cx, cy), 5, (0,0,255), -1)
             cv2.putText(img, f"{cx, cy}", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
             centers.append((cx, cy))
-    return img, centers
-
-img = cv2.imread(path)
-img = cv2.resize(img, (1200, 2000))
-maskedImage = getElements(img)
-contoursImage, centers = getPosition(maskedImage)
-centers.pop(-1)
-print(centers)
-showImages([img, maskedImage, contoursImage])
+    showImages([image, maskedImage, img])
+    return centers
